a9d05cc218c443b83a2cf7cb650e3df94f812554
lsblu <lsblu@hotmail.com>
2022-07-29 16:09:17 -0300

    REQ07

---
+++ src/database/models/category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes, ) => {
+  const Caterogy = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  });
+  return Caterogy;
+}
