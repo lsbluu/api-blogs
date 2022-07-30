c50658ba1a4a035eeee7ef0da22faa3c770c979f
lsblu <lsblu@hotmail.com>
2022-07-30 12:12:25 -0300

    REQ10

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    content: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      },      
+    },
+    published: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updated: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  });
+BlogPost.associate = (models) => {
+  BlogPost.belongsTo(models.User,{
+    foreignKey: 'userId', as: 'user'
+  });
+};
+
+  return BlogPost;
+}
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -22,5 +22,11 @@ module.exports = (sequelize, DataTypes) => {
       
   },{ timestamps: false},);
   
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,{
+      foreignKey: 'userId', as: 'blogposts'
+    });
+  };
+
   return User;
 }
